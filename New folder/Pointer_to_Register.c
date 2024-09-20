8add99e5f4cb8da56f4c0df684ffdae9cf66eb01
Saif <124804475+Nazmus-Saif@users.noreply.github.com>
2024-09-20 11:39:26 +0600

    Delete New folder directory

--- New folder/Pointer_to_Register.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    register int i = 0;
-    int *j;
-    j = &i;
-    printf("%p\n", j); // we cant print the address of a register variable as they stored in cpu memory not in a ram
-}
